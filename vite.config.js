a5f0eb5df40a13dbbef5bb33ecd9dad71201f327
cog25 <git@cog.moe>
2026-06-08 21:21:32 +0900

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,17 @@ import { resolve } from 'node:path';
 
 export default defineConfig({
     plugins: [tailwindcss({ })],
-    rolldownOptions: {
-        input: {
-            main: resolve(import.meta.dirname, 'index.html'),
-            project2: resolve(import.meta.dirname, 'project2/index.html'),
-
-        }
-    },
+    build: {
+        rolldownOptions: {
+            input: {
+                main: resolve(import.meta.dirname, 'index.html'),
+                project1: resolve(import.meta.dirname, 'project-1/index.html'),
+                project2: resolve(import.meta.dirname, 'project-2/index.html'),
+                project3: resolve(import.meta.dirname, 'project-3/index.html'),
+                project4: resolve(import.meta.dirname, 'project-4/index.html'),
+                
+            }
+        },
+    }
 
 });
